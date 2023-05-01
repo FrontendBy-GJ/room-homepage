66993ce2826208cec9e4c17e19177cd02671374f
Genaro Garcia <genaro.dev@outlook.com>
2023-05-01 14:32:59 -0500

    update images path

--- src/data.js
+++ src/data.js
@@ -5,8 +5,8 @@ export const slides = [
     content:
       'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
     images: {
-      mobile: 'public/mobile-image-hero-1.jpg',
-      desktop: 'public/desktop-image-hero-1.jpg',
+      mobile: './public/mobile-image-hero-1.jpg',
+      desktop: './public/desktop-image-hero-1.jpg',
     },
     link: 'Shop now',
   },
@@ -16,8 +16,8 @@ export const slides = [
     content:
       "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     images: {
-      mobile: 'public/mobile-image-hero-2.jpg',
-      desktop: 'public/desktop-image-hero-2.jpg',
+      mobile: './public/mobile-image-hero-2.jpg',
+      desktop: './public/desktop-image-hero-2.jpg',
     },
     link: 'Shop now',
   },
@@ -27,8 +27,8 @@ export const slides = [
     content:
       'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.',
     images: {
-      mobile: 'public/mobile-image-hero-3.jpg',
-      desktop: 'public/desktop-image-hero-3.jpg',
+      mobile: './public/mobile-image-hero-3.jpg',
+      desktop: './public/desktop-image-hero-3.jpg',
     },
     link: 'Shop now',
   },
